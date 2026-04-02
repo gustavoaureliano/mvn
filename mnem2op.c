a87cba6ea8991840a03561662741c25f29c0aca0
Gustavo Rocha Aureliano <gurocha27@gmail.com>
2026-04-02 09:16:18 -0300

    harden cli and parser error handling

--- mnem2op.c
+++ mnem2op.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 
 #define NUM_INST 18
 #define OPERANDO_SIZE 64
@@ -26,6 +25,13 @@ typedef struct {
 	char rotulo[OPERANDO_SIZE];
 } Symbol;
 
+typedef enum {
+	OK = 0,
+	ERR_USAGE = 1,
+	ERR_IO = 2,
+	ERR_PARSE = 3
+} ExitCode;
+
 const Mnemonico mnemtable[NUM_INST] = {
 	{"JP", "0"},
 	{"JZ", "1"},
@@ -43,44 +49,42 @@ const Mnemonico mnemtable[NUM_INST] = {
 	{"GD", "D"},
 	{"PD", "E"},
 	{"SO", "F"},
-	{"@" , "" },
-	{"K" , "" }
+	{"@", ""},
+	{"K", ""}
 };
 
-int is_blank_line(const char *line) {
-	while (*line) {
-		if (!isspace(*line)) {
-			return 0;
-		}
-		line++;
-	}
-	return 1;
-}
-
 int get_inst_index(const char *inst) {
-	// printf("get index:\n");
 	for (int i = 0; i < NUM_INST; i++) {
 		if (strcmp(mnemtable[i].mnemonico, inst) == 0) {
 			return i;
 		}
 	}
+
 	return -1;
 }
 
 int get_symbol_index(const char *text, Symbol symbols[], int size) {
-	// printf("get index symbol:\n");
 	for (int i = 0; i < size; i++) {
-		// printf("%s == %s ?\n", symbol, symbols[i].rotulo);
 		if (strcmp(symbols[i].rotulo, text) == 0) {
-			// printf("yes!\n");
 			return i;
 		}
 	}
+
 	return -1;
 }
 
 int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s arquivo.asm\n", argv[0]);
+		return ERR_USAGE;
+	}
+
 	FILE *fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		perror("fopen");
+		return ERR_IO;
+	}
+
 	char *line = NULL;
 	size_t size = 0;
 	ssize_t nread;
@@ -92,58 +96,74 @@ int main(int argc, char *argv[]) {
 	MVN program[PROGRAM_SIZE] = {0};
 	Symbol symbols[PROGRAM_SIZE] = {0};
 
-	// primeiro passo
 	while ((nread = getline(&line, &size, fp)) != -1 && curr_address < 1000) {
-		token = strtok(line, " \t\n");
-		if (is_blank_line(token))
+		(void)nread;
+		token = strtok(line, " \t\r\n");
+		if (token == NULL) {
 			continue;
-		if (token[0] == ';')
+		}
+		if (token[0] == ';') {
 			continue;
-		if (strcmp(token, "\n") == 0)
-			continue;
+		}
+
 		int inst_index = get_inst_index(token);
 		int symbol_index = -1;
 		uint16_t operando_addr = 0;
 		char *symbol = token;
-		// printf("token: %s; index: %d\n", symbol, inst_index);
+
 		token = strtok(NULL, " \t\r\n");
-		// printf("new token: %s\n", token);
 		if (inst_index < 0) {
+			if (token == NULL) {
+				fprintf(stderr, "Parse error: missing mnemonic after label\n");
+				fclose(fp);
+				free(line);
+				return ERR_PARSE;
+			}
+
 			symbol_index = get_symbol_index(symbol, symbols, symbol_counter);
 			inst_index = get_inst_index(token);
+			if (inst_index < 0) {
+				fprintf(stderr, "Parse error: invalid mnemonic '%s'\n", token);
+				fclose(fp);
+				free(line);
+				return ERR_PARSE;
+			}
 			if (symbol_index >= 0) {
-				// printf("updating symbol address: %s -> %03X\n", symbol, curr_address);
 				symbols[symbol_index].address = curr_address;
 				symbols[symbol_index].is_defined = 1;
-
 			} else {
 				snprintf(symbols[symbol_counter].rotulo, OPERANDO_SIZE, "%s", symbol);
-				symbols[symbol_counter].address = curr_address; // shouldn't be needed
+				symbols[symbol_counter].address = curr_address;
 				symbols[symbol_counter].is_defined = 1;
 				symbol_counter++;
 			}
-			symbol = token;
 			token = strtok(NULL, " \t\r\n");
 		}
+
+		if (token == NULL) {
+			fprintf(stderr, "Parse error: missing operand\n");
+			fclose(fp);
+			free(line);
+			return ERR_PARSE;
+		}
+
 		switch (inst_index) {
 			case 16:
 				switch (token[0]) {
 					case '/':
-						// printf("hex: %s\n", token);
 						curr_address = strtoul(&token[1], NULL, 16);
 						break;
 					case '=':
-						// printf("decimal: %s\n", token);
 						curr_address = strtoul(&token[1], NULL, 10);
 						break;
 					default:
-						// label
 						symbol_index = get_symbol_index(token, symbols, symbol_counter);
 						if (symbol_index >= 0) {
-							if (symbols[symbol_index].is_defined > 0)
+							if (symbols[symbol_index].is_defined > 0) {
 								snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%03X", symbols[symbol_index].address);
-							else
+							} else {
 								snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%s", token);
+							}
 							break;
 						}
 						snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%s", token);
@@ -158,23 +178,21 @@ int main(int argc, char *argv[]) {
 				operando_addr = 0;
 				switch (token[0]) {
 					case '/':
-						// printf("hex: %s\n", token);
 						operando_addr = strtoul(&token[1], NULL, 16);
 						snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%04X", operando_addr);
 						break;
 					case '=':
-						// printf("decimal: %s\n", token);
 						operando_addr = strtoul(&token[1], NULL, 10);
 						snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%04X", operando_addr);
 						break;
 					default:
-						// label
 						symbol_index = get_symbol_index(token, symbols, symbol_counter);
 						if (symbol_index >= 0) {
-							if (symbols[symbol_index].is_defined > 0)
+							if (symbols[symbol_index].is_defined > 0) {
 								snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%04X", symbols[symbol_index].address);
-							else
+							} else {
 								snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%s", token);
+							}
 							break;
 						}
 						snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%s", token);
@@ -190,23 +208,21 @@ int main(int argc, char *argv[]) {
 				program[inst_counter].opcode = inst_index;
 				switch (token[0]) {
 					case '/':
-						// printf("hex: %s\n", token);
 						operando_addr = strtoul(&token[1], NULL, 16);
 						snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%03X", operando_addr);
 						break;
 					case '=':
-						// printf("decimal: %s\n", token);
 						operando_addr = strtoul(&token[1], NULL, 10);
 						snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%03X", operando_addr);
 						break;
 					default:
-						// label
 						symbol_index = get_symbol_index(token, symbols, symbol_counter);
 						if (symbol_index >= 0) {
-							if (symbols[symbol_index].is_defined > 0)
+							if (symbols[symbol_index].is_defined > 0) {
 								snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%03X", symbols[symbol_index].address);
-							else
+							} else {
 								snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%s", token);
+							}
 							break;
 						}
 						snprintf(program[inst_counter].operando, OPERANDO_SIZE, "%s", token);
@@ -219,24 +235,7 @@ int main(int argc, char *argv[]) {
 				break;
 		}
 	}
-	// segundo passo
-	// back to the start of the file
-	fseek(fp, 0L, SEEK_SET);
-	// while ((nread = getline(&line, &size, fp)) != -1 && curr_address < 1000) {
-	// 	printf("%s", line);
-	// }
-	// printf("\n");
-	// printf("MVN(%d):\n", inst_counter);
-	// for (int i = 0; i < symboltblcount; i++) {
-	//  printf("(%d) %s -> %s\n", i, symboltbl[i].symbol, symboltbl[i].address);
-	//  // printf("%d: {%s, %s, %s, %s}", 2*i, mvn[i][0], mvn[i][1], mvn[i][2],
-	//  mvn[i][3])
-	// }
-	//
-	// for (int i = 0; i < inst_counter; i++) {
-	// 	printf("%04X %d %s\n", program[i].address, program[i].opcode, program[i].operando);
-	// }
-	// printf("memory:\n");
+
 	for (int i = 0; i < inst_counter; i++) {
 		printf("%04X ", program[i].address);
 		if (program[i].opcode < 0) {
@@ -250,11 +249,8 @@ int main(int argc, char *argv[]) {
 		}
 		printf("%01X%03X\n", program[i].opcode, symbols[idx].address);
 	}
-	// printf("symbols(%d):\n", symbol_counter);
-	// for (int i = 0; i < symbol_counter; i++) {
-	// 	printf("%04X %s\n", symbols[i].address, symbols[i].rotulo);
-	// }
+
 	fclose(fp);
 	free(line);
-	return 0;
+	return OK;
 }
